1b89d3bd6d795de37cc6924917a8b0ef640259d4
Борис Женихов <boriszenihov@MacBook-Pro-Boris.local>
2021-09-20 18:24:09 +0600

    Add class Bank

--- utils/engine.py
+++ utils/engine.py
@@ -1,24 +1,46 @@
-class Formatted(object):
-    def __init__(self, value=0):
-        self.money = value
+import locale
 
-    def money(self):
-        return self.money
+locale.setlocale(locale.LC_ALL, "kk_KZ")
 
+
+class Bank(object):
+    def __init__(self,
+                 _amount=0,
+                 _loan=0,
+                 _capital=0
+                 ):
+        self._amount = _amount
+        self._loan = _loan
+        self._capital = _capital
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, new):
+        self._amount = new
+
+    @property
+    def loan(self):
+        return self._loan
+
+    @loan.setter
+    def loan(self, new):
+        self._loan = new
+
+    @property
     def capital(self):
+        return ((self._amount - self._loan) / 100) * self._capital
 
-        # self.money = (self.money / 100) * 60
-        self.capital = (self.money / 100) * 60
-        return self.capital
+    @capital.setter
+    def capital(self, value):
+        self._capital = value
 
-    def currency(self):
-        return locale.currency(self.money)
+    #
+    # def currency(self):
+    #     return locale.currency(self.money)
 
-    def add(self, value):
-        self.money = value
-        if isinstance(value, int):
-            self.money = value
-
-    def remove(self, value):
-        if isinstance(value, int):
-            self.money -= value
+b = Bank()
+b.amount=2
+print(b.amount)
