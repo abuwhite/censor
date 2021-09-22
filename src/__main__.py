9860a4f74493a15f390f240318558de9f48bcaab
Борис Женихов <boriszenihov@MacBook-Pro-Boris.local>
2021-09-22 15:44:05 +0600

    Refactoring

--- src/__main__.py
+++ src/__main__.py
@@ -5,57 +5,44 @@ from flask import (
     redirect
 )
 
-from src.utils import engine
+from src.utils.bank import Bank, get_month_amount
+from babel import numbers
 
 app = Flask(__name__)
+
+user = Bank()
+
+
+def currency(num):
+    return numbers.format_currency(num, 'KZT', locale='kk_KZ')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', amount=200000)
-
-
-@app.route('/info')
-def test():
-    return render_template('info.html')
+    return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    # print('[AMOUNT FORM]', request.form['amount'], type(request.form['amount']))
-    # print('[LOAN FORM]', request.form['loan'], type(request.form['loan']))
-    # print('[CAPITAL FORM]', request.form['capital'], type(request.form['capital']))
-    amount = int(request.form['amount'])
-    loan = int(request.form['loan'])
-    capital = int(request.form['capital'])
+    global user
+    user = Bank(_amount=int(request.form['amount']),
+                _loan=int(request.form['loan']),
+                _percent=int(request.form['capital'])
+                )
 
-    bank = engine.Bank(
-        _amount=amount,
-        _loan=loan,
-        _capital=capital
-    )
-
-    print('[AMOUNT]', bank.amount)
-    print('[LOAN]', bank.loan)
-    print('[CAPITAL]', bank.capital)
-
-    # bank.capital = 90
-    #
-    # print('[NEW CAPITAL]', bank.capital)
+    return redirect('/info')
 
 
-    # if request.form['amount'] == '':
-    #     bank.add(0)
-    # else:
-    #     bank.add(int(request.form['amount']))
-    #
-    # if request.form['loan'] == '':
-    #     bank.remove(0)
-    # else:
-    #     bank.remove(int(request.form['loan']))
-
-
-    return redirect('/')
+@app.route('/info')
+def test():
+    print(user.percent)
+    return render_template('info.html',
+                           percent=user.percent,
+                           month=currency(get_month_amount(user.capital)),
+                           loan=currency(user.loan),
+                           capital=currency(user.capital),
+                           amount=currency(user.free)
+                           )
 
 
 if __name__ == '__main__':
