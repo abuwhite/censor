44e14c61c174674fa6b4be6bd38865647ef897f2
Борис Женихов <boriszenihov@MacBook-Pro-Boris.local>
2021-09-22 15:44:19 +0600

    Refactoring

--- src/utils/bank.py
+++ src/utils/bank.py
@@ -1,21 +1,16 @@
-import locale
-
-locale.setlocale(locale.LC_ALL, "kk_KZ")
-
-
 class Bank(object):
     def __init__(self,
                  _amount=0,
                  _loan=0,
-                 _capital=60
+                 _percent=10
                  ):
         self._amount = _amount
         self._loan = _loan
-        self._capital = _capital
+        self._percent = _percent
 
     @property
     def amount(self):
-        return self._amount
+        return int(self._amount)
 
     @amount.setter
     def amount(self, new):
@@ -23,20 +18,24 @@ class Bank(object):
 
     @property
     def loan(self):
-        return self._loan
+        return int(self._loan)
 
     @loan.setter
     def loan(self, new):
         self._loan = new
 
     @property
+    def percent(self):
+        return self._percent
+
+    @property
     def capital(self):
-        return ((self._amount - self._loan) / 100) * self._capital
+        return int(((self._amount - self._loan) / 100) * self._percent)
 
-    @capital.setter
-    def capital(self, value):
-        self._capital = value
+    @property
+    def free(self):
+        return self._amount - self._loan - self.capital
 
-    #
-    # def currency(self):
-    #     return locale.currency(self.money)
+
+def get_month_amount(num):
+    return num * 12
