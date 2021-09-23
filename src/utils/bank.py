c2edba2f10d5e79f0076145fb0b14d361acb2417
Борис Женихов <znhv@yandex.com>
2021-09-23 15:05:48 +0600

    Refactoring

--- src/utils/bank.py
+++ src/utils/bank.py
@@ -1,41 +1,52 @@
+"""Модуль Банка."""
+
+
 class Bank(object):
     def __init__(self,
-                 _amount=0,
-                 _loan=0,
+                 _income=0,
+                 _expenses=0,
                  _percent=10
                  ):
-        self._amount = _amount
-        self._loan = _loan
+        """
+
+        :param _income: Доход
+        :param _expenses: Расход
+        :param _percent: Процент
+        """
+        self._income = _income
+        self._expenses = _expenses
         self._percent = _percent
 
     @property
-    def amount(self):
-        return int(self._amount)
-
-    @amount.setter
-    def amount(self, new):
-        self._amount = new
+    def income(self):
+        """Получить сумму дохода."""
+        return int(self._income)
 
     @property
-    def loan(self):
-        return int(self._loan)
-
-    @loan.setter
-    def loan(self, new):
-        self._loan = new
+    def expenses(self):
+        """Получить сумму расходов."""
+        return int(self._expenses)
 
     @property
     def percent(self):
+        """Получить процент."""
         return self._percent
 
     @property
     def capital(self):
-        return int(((self._amount - self._loan) / 100) * self._percent)
+        """Получить сумму свободных средств по формуле
+
+        ((Доход - Расход) / 100) * Процент
+
+        """
+        return int(((self._income - self._expenses) / 100) * self._percent)
 
     @property
     def free(self):
-        return self._amount - self._loan - self.capital
+        """Получить свободную сумму после всех вычетов."""
+        return self._income - self._expenses - self.capital
 
-
-def get_month_amount(num):
-    return num * 12
+    @property
+    def capital_year(self):
+        """Получить сумму накоплений за 6 месяцев."""
+        return self.capital * 6
